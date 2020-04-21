4a605b5e9da773fc76e685a432c1406c15bc4d56
Zhuohan Li <zhuohan123@vip.qq.com>
2020-04-21 00:37:26 -0700

    add pytorch distributed init

--- test_xab.py
+++ test_xab.py
@@ -1,26 +1,31 @@
 import torch
+import torch.distributed as dist
 import mpu
 import argparse
 import random
 
 
 def distributed_main(process_idx, args):
-    mpu.initialize_model_parallel(args.model_parallel_size)
-    args.device_id = process_idx
-    print(process_idx, args)
+    args.distributed_rank = process_idx
+    dist.init_process_group(
+        backend='nccl',
+        init_method=args.distributed_init_method,
+        world_size=args.distributed_world_size,
+        rank=args.distributed_rank,
+    )
+    mpu.initialize_model_parallel(args.distributed_world_size)
+    print(args)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test model parallel speed with program Y=XAB')
-    parser.add_argument('model_parallel_size', metavar='N', type=int, nargs='+',
-                        help='Model parallel size')
+    parser.add_argument('distributed_world_size', metavar='N', type=int, help='Model parallel size')
     args = parser.parse_args()
     port = random.randint(10000, 20000)
     args.distributed_init_method = 'tcp://localhost:{port}'.format(port=port)
-    args.distributed_rank = None
 
     torch.multiprocessing.spawn(
         distributed_main,
         args=(args,),
-        nprocs=args.model_parallel_size
+        nprocs=args.distributed_world_size
     )
