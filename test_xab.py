b95753e932d76868c35e82edf438751db9ea861d
Zhuohan Li <zhuohan123@vip.qq.com>
2020-04-21 00:47:17 -0700

    fix random seed and set device

--- test_xab.py
+++ test_xab.py
@@ -2,7 +2,15 @@ import torch
 import torch.distributed as dist
 import mpu
 import argparse
+import numpy as np
 import random
+
+
+def set_random_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    mpu.model_parallel_cuda_manual_seed(seed)
 
 
 def distributed_main(process_idx, args):
@@ -13,8 +21,13 @@ def distributed_main(process_idx, args):
         world_size=args.distributed_world_size,
         rank=args.distributed_rank,
     )
+    torch.cuda.set_device(args.distributed_rank)
+    # perform a dummy all-reduce to initialize the NCCL communicator
+    dist.all_reduce(torch.zeros(1).cuda())
     mpu.initialize_model_parallel(args.distributed_world_size)
-    print(args)
+    set_random_seed(1337)
+    x = torch.rand(5)
+    print(x, args)
 
 
 if __name__ == "__main__":
@@ -23,7 +36,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
     port = random.randint(10000, 20000)
     args.distributed_init_method = 'tcp://localhost:{port}'.format(port=port)
-
+    assert torch.cuda.is_available()
     torch.multiprocessing.spawn(
         distributed_main,
         args=(args,),
