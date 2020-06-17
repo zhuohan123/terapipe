8ad1d7a9ca829bf33662a674e1fc1916c303ea88
Zhuohan Li <zhuohan123@vip.qq.com>
2020-06-17 15:08:35 -0700

    refactor codes

--- test_transformer.py
+++ test_transformer.py
@@ -1,5 +1,4 @@
 import time
-import math
 import random
 import numpy as np
 import torch
@@ -7,8 +6,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import mpu
 import itertools
-
-NEG_INF = -1e10
+import sys
+from transformer_models import (
+    TransformerConfig, TransformerLayer,
+    SingleDeviceTransformer, PipelinedTransformer
+)
 
 
 def set_random_seed(seed):
@@ -18,151 +20,29 @@ def set_random_seed(seed):
     # mpu.model_parallel_cuda_manual_seed(seed)
 
 
-class MultiheadLMAttentionWithCache(nn.Module):
-    def __init__(self, embed_dim, num_heads, bias=True):
-        super().__init__()
-
-        self.embed_dim = embed_dim
-
-        self.k_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
-        self.v_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
-        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
-        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
-
-        nn.init.xavier_uniform_(self.k_proj.weight, gain=1 / math.sqrt(2))
-        nn.init.xavier_uniform_(self.v_proj.weight, gain=1 / math.sqrt(2))
-        nn.init.xavier_uniform_(self.q_proj.weight, gain=1 / math.sqrt(2))
-        nn.init.xavier_uniform_(self.out_proj.weight)
-        if self.out_proj.bias is not None:
-            nn.init.constant_(self.out_proj.bias, 0.)
-
-        self.num_heads = num_heads
-        self.head_dim = embed_dim // num_heads
-        assert (
-            self.head_dim * num_heads == self.embed_dim
-        ), "embed_dim must be divisible by num_heads"
-        self.scaling = self.head_dim ** -0.5
-
-    def forward(self, x, cache=None):
-        tgt_len, bsz, embed_dim = x.size()
-        assert embed_dim == self.embed_dim
-        q = self.q_proj(x)
-        k = self.k_proj(x)
-        v = self.v_proj(x)
-        q *= self.scaling
-        q = (
-            q.contiguous()
-            .view(tgt_len, bsz * self.num_heads, self.head_dim)
-            .transpose(0, 1)
-        )
-        k = (
-            k.contiguous()
-            .view(tgt_len, bsz * self.num_heads, self.head_dim)
-            .transpose(0, 1)
-        )
-        v = (
-            v.contiguous()
-            .view(tgt_len, bsz * self.num_heads, self.head_dim)
-            .transpose(0, 1)
-        )
-        attn_mask = x.new_full((tgt_len, tgt_len), NEG_INF).triu(1)
-        src_len = tgt_len
-        if cache is not None:
-            cache_len = cache["k"].size()[1]
-            k = torch.cat([cache["k"], k], dim=1)
-            v = torch.cat([cache["v"], v], dim=1)
-            attn_mask = torch.cat([x.new_zeros(tgt_len, cache_len), attn_mask], dim=1)
-            src_len += cache_len
-
-        new_cache = {
-            "k": k,
-            "v": v,
-        }
-
-        attn_weights = torch.bmm(q, k.transpose(1, 2))
-        assert list(attn_weights.size()) == [bsz * self.num_heads, tgt_len, src_len]
-        attn_weights += attn_mask[None, :, :]
-        attn_probs = F.softmax(attn_weights, dim=-1, dtype=torch.float32).type_as(attn_weights)
-        attn = torch.bmm(attn_probs, v)
-        attn = attn.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
-        attn = self.out_proj(attn)
-
-        return attn, new_cache
-
-
-class TransformerLayer(nn.Module):
-    def __init__(self, embedding_dim, ffn_embedding_dim, num_attention_heads):
-        super().__init__()
-        self.attn_ln = nn.LayerNorm(embedding_dim)
-        self.attn = MultiheadLMAttentionWithCache(embedding_dim, num_attention_heads)
-        self.fc_ln = nn.LayerNorm(embedding_dim)
-        self.fc1 = nn.Linear(embedding_dim, ffn_embedding_dim)
-        self.fc2 = nn.Linear(ffn_embedding_dim, embedding_dim)
-
-    def forward(self, x, attn_cache=None):
-        seq_len, batch_size, _ = x.size()
-        y = x
-        x = self.attn_ln(x)
-        x, new_attn_cache = self.attn(x, attn_cache)
-        x = y + x
-        y = x
-        x = self.fc_ln(x)
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = y + x
-        return x, new_attn_cache
-
-
-class SingleDeviceTransformer(nn.Module):
-    def __init__(self, transformer_layers):
-        super().__init__()
-        self.layers = nn.ModuleList(transformer_layers)
-
-    def forward(self, x, attn_caches=None):
-        if attn_caches is None:
-            attn_caches = [None] * len(self.layers)
-        new_attn_caches = []
-        for layer, attn_cache in zip(self.layers, attn_caches):
-            x, new_attn_cache = layer(x, attn_cache)
-            new_attn_caches.append(new_attn_cache)
-        return x, new_attn_caches
-
-
-class PipelinedTransformer(nn.Module):
-    def __init__(self, nested_transformer_layers):
-        super().__init__()
-        self.n_devices = len(nested_transformer_layers)
-        assert self.n_devices <= torch.cuda.device_count()
-        self.single_device_transformers = nn.ModuleList([
-            SingleDeviceTransformer(transformer_layers).to(torch.device(i))
-            for i, transformer_layers in enumerate(nested_transformer_layers)
-        ])
-
-    def forward(self, segmented_xs):
-        n_segments = len(segmented_xs)
-        n_timesteps = len(segmented_xs) + self.n_devices - 1
-        caches = [None] * self.n_devices
-        for t in range(n_timesteps):
-            for i in range(self.n_devices):
-                if 0 <= t - i < n_segments:
-                    x = segmented_xs[t - i].to(torch.device(i))
-                    x, cache = self.single_device_transformers[i](x, caches[i])
-                    caches[i] = cache
-                    segmented_xs[t - i] = x
-
-        return segmented_xs
-
-
-def uniform_slice(x, seq_len, n_slices):
-    slice_x = []
+def uniform_slice_x(x, n_slices):
+    seq_len = x.size()[0]
+    sliced_x = []
     start_index = 0
     for i in range(n_slices):
         seq_len_slice = seq_len // n_slices + int(i < seq_len % n_slices)
-        slice_x.append(x[start_index:start_index + seq_len_slice])
+        sliced_x.append(x[start_index:start_index + seq_len_slice].detach())
         start_index += seq_len_slice
     assert start_index == seq_len
-    return slice_x
+    return sliced_x
+
+
+def uniform_slice_layers(transformer_layers, n_devices=None):
+    n_layers = len(transformer_layers)
+    n_devices = n_devices if n_devices else torch.cuda.device_count()
+    nested_layers = []
+    layer_idx = 0
+    for i in range(n_devices):
+        n_layers_device = n_layers // n_devices + int(i < n_layers % n_devices)
+        nested_layers.append(transformer_layers[layer_idx:layer_idx + n_layers_device])
+        layer_idx += n_layers_device
+    assert layer_idx == n_layers
+    return nested_layers
 
 
 def unit_test_forward_time(
@@ -222,102 +102,86 @@ def grid_search_forward_time():
         print(n_layers, embedding_dim, seq_len, cache_times, this_step_time)
 
 
-def compare_single_device_and_pipeline():
-    time_testing_steps = 100
-    batch_size = 1
-    seq_len = 1024
-    n_layers = 12
-    embedding_dim = 768
-    ffn_embedding_dim = embedding_dim * 4
-    num_attention_heads = 12
-    transformer_layers = [
-        TransformerLayer(embedding_dim, ffn_embedding_dim, num_attention_heads)
-        for _ in range(n_layers)
-    ]
-    x = torch.randn(seq_len, batch_size, embedding_dim).cuda(0)
+def single_device_time(config: TransformerConfig, n_testing_steps=10):
+    transformer_layers, x = config.create_layers_and_inputs()
+    x = x.cuda(0)
     single_device_transformer = SingleDeviceTransformer(transformer_layers).cuda(0)
+    for t in range(2):
+        single_device_transformer.zero_grad()
+        y, _ = single_device_transformer(x)
+        loss = torch.mean(y)
+        loss.backward()
     torch.cuda.synchronize()
     start = time.time()
-    for t in range(time_testing_steps):
+    for t in range(n_testing_steps):
         single_device_transformer.zero_grad()
         y, _ = single_device_transformer(x)
         loss = torch.mean(y)
         loss.backward()
     torch.cuda.synchronize()
     duration = time.time() - start
-    print("single_device_time (per step):", duration / time_testing_steps)
+    return duration / n_testing_steps
 
-    n_devices = torch.cuda.device_count()
-    nested_layers = []
-    layer_idx = 0
-    for i in range(n_devices):
-        n_layers_device = n_layers // n_devices + int(i < n_layers % n_devices)
-        nested_layers.append(transformer_layers[layer_idx:layer_idx + n_layers_device])
-        layer_idx += n_layers_device
+
+def gpipe_time(config: TransformerConfig, n_testing_steps=10):
+    transformer_layers, x = config.create_layers_and_inputs()
+    x = x.cuda(0)
+    nested_layers = uniform_slice_layers(transformer_layers)
     pipelined_transformer = PipelinedTransformer(nested_layers)
-
-    slice_x = uniform_slice(x, seq_len, 16)
-    torch.cuda.synchronize()
-    start = time.time()
-    for t in range(time_testing_steps):
+    for t in range(2):
         pipelined_transformer.zero_grad()
-        y_pipelined = pipelined_transformer(slice_x)
+        y_pipelined = pipelined_transformer([x])
         loss = torch.mean(torch.cat(y_pipelined, dim=0))
         loss.backward()
     torch.cuda.synchronize()
-    duration = time.time() - start
-    print("pipelined_time (per step):", duration / time_testing_steps)
-
-
-def compare_gpipe_and_pipeline():
-    time_testing_steps = 10
-    batch_size = 1
-    seq_len = 1024
-    n_layers = 72
-    embedding_dim = 3072
-    ffn_embedding_dim = embedding_dim * 4
-    num_attention_heads = 32
-    transformer_layers = [
-        TransformerLayer(embedding_dim, ffn_embedding_dim, num_attention_heads)
-        for _ in range(n_layers)
-    ]
-    x = torch.randn(seq_len, batch_size, embedding_dim).cuda(0)
-
-    n_devices = torch.cuda.device_count()
-    nested_layers = []
-    layer_idx = 0
-    for i in range(n_devices):
-        n_layers_device = n_layers // n_devices + int(i < n_layers % n_devices)
-        nested_layers.append(transformer_layers[layer_idx:layer_idx + n_layers_device])
-        layer_idx += n_layers_device
-    assert layer_idx == n_layers
-    pipelined_transformer = PipelinedTransformer(nested_layers)
-
-    torch.cuda.synchronize()
     start = time.time()
-    for t in range(time_testing_steps):
+    for t in range(n_testing_steps):
         pipelined_transformer.zero_grad()
         y_pipelined = pipelined_transformer([x])
         loss = torch.mean(torch.cat(y_pipelined, dim=0))
         loss.backward()
     torch.cuda.synchronize()
     duration = time.time() - start
-    print("gpipe_time (per step):", duration / time_testing_steps)
+    return duration / n_testing_steps
 
-    slice_x = uniform_slice(x, seq_len, 16)
+
+def seqpipe_time(config: TransformerConfig, n_testing_steps=10, n_slices=8):
+    transformer_layers, x = config.create_layers_and_inputs()
+    nested_layers = uniform_slice_layers(transformer_layers)
+    pipelined_transformer = PipelinedTransformer(nested_layers)
+    sliced_x = uniform_slice_x(x.cuda(0), config.seq_len, n_slices)
+    for t in range(2):
+        pipelined_transformer.zero_grad()
+        y_pipelined = pipelined_transformer(sliced_x)
+        loss = torch.mean(torch.cat(y_pipelined, dim=0))
+        loss.backward()
     torch.cuda.synchronize()
     start = time.time()
-    for t in range(time_testing_steps):
+    for t in range(n_testing_steps):
         pipelined_transformer.zero_grad()
-        y_pipelined = pipelined_transformer(slice_x)
+        y_pipelined = pipelined_transformer(sliced_x)
         loss = torch.mean(torch.cat(y_pipelined, dim=0))
         loss.backward()
     torch.cuda.synchronize()
     duration = time.time() - start
-    print("pipelined_time (per step):", duration / time_testing_steps)
+    return duration / n_testing_steps
 
 
 if __name__ == "__main__":
     set_random_seed(0)
     # grid_search_forward_time()
-    compare_gpipe_and_pipeline()
+    config = TransformerConfig(
+        batch_size=1,
+        seq_len=1024,
+        n_layers=72,
+        embedding_dim=2048,
+    )
+    assert len(sys.argv) > 1
+    if sys.argv[1] == "gridsearch":
+        grid_search_forward_time()
+    elif sys.argv[1] == "single":
+        print("single_device (s/it):", single_device_time(config))
+    elif sys.argv[1] == "gpipe":
+        print("gpipe (s/it):", gpipe_time(config))
+    elif sys.argv[1] == "seqpipe":
+        print("seqpipe (s/it):", seqpipe_time(config))
---
+++ transformer_models.py
@@ -0,0 +1,172 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+NEG_INF = -1e10
+
+
+class TransformerConfig:
+    def __init__(
+        self,
+        batch_size=1,
+        seq_len=1024,
+        n_layers=12,
+        embedding_dim=768,
+        ffn_embedding_dim=None,
+        num_attention_heads=None,
+    ):
+        self.batch_size = batch_size
+        self.seq_len = seq_len
+        self.n_layers = n_layers
+        self.embedding_dim = embedding_dim
+        self.ffn_embedding_dim = ffn_embedding_dim if ffn_embedding_dim else embedding_dim * 4
+        self.num_attention_heads = num_attention_heads if num_attention_heads else embedding_dim // 64
+
+    def create_layers_and_inputs(self):
+        transformer_layers = [
+            TransformerLayer(
+                self.embedding_dim,
+                self.ffn_embedding_dim,
+                self.num_attention_heads
+            )
+            for _ in range(self.n_layers)
+        ]
+        x = torch.randn(self.seq_len, self.batch_size, self.embedding_dim)
+        return transformer_layers, x
+
+
+class MultiheadLMAttentionWithCache(nn.Module):
+    def __init__(self, embed_dim, num_heads, bias=True):
+        super().__init__()
+
+        self.embed_dim = embed_dim
+
+        self.k_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.v_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+
+        nn.init.xavier_uniform_(self.k_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.v_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.q_proj.weight, gain=1 / math.sqrt(2))
+        nn.init.xavier_uniform_(self.out_proj.weight)
+        if self.out_proj.bias is not None:
+            nn.init.constant_(self.out_proj.bias, 0.)
+
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+        assert (
+            self.head_dim * num_heads == self.embed_dim
+        ), "embed_dim must be divisible by num_heads"
+        self.scaling = self.head_dim ** -0.5
+
+    def forward(self, x, cache=None):
+        tgt_len, bsz, embed_dim = x.size()
+        assert embed_dim == self.embed_dim
+        q = self.q_proj(x)
+        k = self.k_proj(x)
+        v = self.v_proj(x)
+        q *= self.scaling
+        q = (
+            q.contiguous()
+            .view(tgt_len, bsz * self.num_heads, self.head_dim)
+            .transpose(0, 1)
+        )
+        k = (
+            k.contiguous()
+            .view(tgt_len, bsz * self.num_heads, self.head_dim)
+            .transpose(0, 1)
+        )
+        v = (
+            v.contiguous()
+            .view(tgt_len, bsz * self.num_heads, self.head_dim)
+            .transpose(0, 1)
+        )
+        attn_mask = x.new_full((tgt_len, tgt_len), NEG_INF).triu(1)
+        src_len = tgt_len
+        if cache is not None:
+            cache_len = cache["k"].size()[1]
+            k = torch.cat([cache["k"], k], dim=1)
+            v = torch.cat([cache["v"], v], dim=1)
+            attn_mask = torch.cat([x.new_zeros(tgt_len, cache_len), attn_mask], dim=1)
+            src_len += cache_len
+
+        new_cache = {
+            "k": k,
+            "v": v,
+        }
+
+        attn_weights = torch.bmm(q, k.transpose(1, 2))
+        assert list(attn_weights.size()) == [bsz * self.num_heads, tgt_len, src_len]
+        attn_weights += attn_mask[None, :, :]
+        attn_probs = F.softmax(attn_weights, dim=-1, dtype=torch.float32).type_as(attn_weights)
+        attn = torch.bmm(attn_probs, v)
+        attn = attn.transpose(0, 1).contiguous().view(tgt_len, bsz, embed_dim)
+        attn = self.out_proj(attn)
+
+        return attn, new_cache
+
+
+class TransformerLayer(nn.Module):
+    def __init__(self, embedding_dim, ffn_embedding_dim, num_attention_heads):
+        super().__init__()
+        self.attn_ln = nn.LayerNorm(embedding_dim)
+        self.attn = MultiheadLMAttentionWithCache(embedding_dim, num_attention_heads)
+        self.fc_ln = nn.LayerNorm(embedding_dim)
+        self.fc1 = nn.Linear(embedding_dim, ffn_embedding_dim)
+        self.fc2 = nn.Linear(ffn_embedding_dim, embedding_dim)
+
+    def forward(self, x, attn_cache=None):
+        seq_len, batch_size, _ = x.size()
+        y = x
+        x = self.attn_ln(x)
+        x, new_attn_cache = self.attn(x, attn_cache)
+        x = y + x
+        y = x
+        x = self.fc_ln(x)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = y + x
+        return x, new_attn_cache
+
+
+class SingleDeviceTransformer(nn.Module):
+    def __init__(self, transformer_layers):
+        super().__init__()
+        self.layers = nn.ModuleList(transformer_layers)
+
+    def forward(self, x, attn_caches=None):
+        if attn_caches is None:
+            attn_caches = [None] * len(self.layers)
+        new_attn_caches = []
+        for layer, attn_cache in zip(self.layers, attn_caches):
+            x, new_attn_cache = layer(x, attn_cache)
+            new_attn_caches.append(new_attn_cache)
+        return x, new_attn_caches
+
+
+class PipelinedTransformer(nn.Module):
+    def __init__(self, nested_transformer_layers):
+        super().__init__()
+        self.n_devices = len(nested_transformer_layers)
+        assert self.n_devices <= torch.cuda.device_count()
+        self.single_device_transformers = nn.ModuleList([
+            SingleDeviceTransformer(transformer_layers).to(torch.device(i))
+            for i, transformer_layers in enumerate(nested_transformer_layers)
+        ])
+
+    def forward(self, segmented_xs):
+        n_segments = len(segmented_xs)
+        n_timesteps = len(segmented_xs) + self.n_devices - 1
+        caches = [None] * self.n_devices
+        for t in range(n_timesteps):
+            for i in range(self.n_devices):
+                if 0 <= t - i < n_segments:
+                    x = segmented_xs[t - i].to(torch.device(i), non_blocking=True)
+                    x, cache = self.single_device_transformers[i](x, caches[i])
+                    caches[i] = cache
+                    segmented_xs[t - i] = x
+
+        return segmented_xs
