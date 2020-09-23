d8bda060f1ecc434db088084df3af50d61e9ca08
Siyuan <suquark@gmail.com>
2020-09-23 01:29:34 -0700

    match the default settings

--- transformer_models.cpp
+++ transformer_models.cpp
@@ -224,8 +224,10 @@ struct MultiDeviceGPT : torch::nn::Module {
 
 int main() {
   int batch_size = 1;
-  int seq_len = 1024;
-  int embedding_dim = 512;
+  int seq_len = 2048;
+  int embedding_dim = 3072;
+  int n_slices = 16;
+  int n_layers = 6;
 
   auto options = torch::TensorOptions().dtype(torch::kFloat32).device(torch::kCUDA, 0).requires_grad(false);
   torch::Tensor x = torch::randn({seq_len, batch_size, embedding_dim}, options);
@@ -236,10 +238,10 @@ int main() {
   // std::cout << fake_loss << std::endl;
   // fake_loss.backward();
 
-  MultiDeviceGPT gpt_m(1, 12, embedding_dim, embedding_dim / 64, embedding_dim * 4);
-  for (int i = 0; i < 20; i++) {
+  MultiDeviceGPT gpt_m(1, n_layers, embedding_dim, embedding_dim / 64, embedding_dim * 4);
+  for (int i = 0; i < 10; i++) {
     gpt_m.zero_grad();
-    torch::Tensor t = gpt_m.forward(x, 16);
+    torch::Tensor t = gpt_m.forward(x, n_slices);
     auto fake_loss = t.mean();
     std::cout << fake_loss << std::endl;
     fake_loss.backward();
