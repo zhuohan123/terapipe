251bf04ad45b84fe2d456d3addc8a9fff947d98b
Siyuan <suquark@gmail.com>
2020-09-22 23:46:32 -0700

    fulfill multi device GPT

--- transformer_models.cpp
+++ transformer_models.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <cstdint>
+#include <thread>
 #include <torch/torch.h>
 
 #include "queue.h"
@@ -14,6 +15,8 @@ struct AttentionCache {
 
 using AttentionResultTuple = std::tuple<torch::Tensor, std::unique_ptr<AttentionCache>>;
 using SingleDeviceAttentionTuple = std::tuple<torch::Tensor, std::vector<std::unique_ptr<AttentionCache>>>;
+using PacketType = std::pair<int, torch::Tensor>;
+using PipelineDataQueue = ConsumerProducerQueue<PacketType>;
 
 struct MultiheadLMAttentionWithCache : torch::nn::Module {
   MultiheadLMAttentionWithCache(int embed_dim, int num_heads, const torch::Device &device)
@@ -134,20 +137,85 @@ struct SingleDeviceGPT : torch::nn::Module {
   torch::nn::ModuleList layers;
 };
 
+void local_pipeline_stage(std::shared_ptr<SingleDeviceGPT> model, PipelineDataQueue &in_queue,
+                          PipelineDataQueue &out_queue, torch::Device device) {
+  std::vector<std::unique_ptr<AttentionCache>> caches;
+  torch::cuda::CUDAGuard g(device);
+  while (true) {
+    PacketType packet;
+    in_queue.consume(packet);
+    switch (packet.first) {
+    case 0:
+      // received the signal for new batch. pass it down to the pipeline and clear local caches.
+      caches.clear();
+      out_queue.add(packet);
+      break;
+    case 1:
+      torch::Tensor x = packet.second.to(device, torch::kFloat32, /*non_blocking=*/true);
+      auto output = model.forward(x, caches);
+      caches = std::move(std::get<1>(output));
+      out_queue.add(std::get<0>(output));
+      break;
+    case 2:
+      // received the stop signal. pass it down to the pipeline and return.
+      out_queue.add(packet);
+      return;
+    default:
+      // TODO: fail the program here
+      std::cerr << "Unknown message type: " << msg << std::endl;
+      break;
+    }
+  }
+}
+
 struct MultiDeviceGPT : torch::nn::Module {
-  MultiDeviceGPT(int n_devices, int n_layers, int embedding_dim, int num_attention_heads, int ffn_embedding_dim) {
+  MultiDeviceGPT(int n_devices, int n_layers, int embedding_dim, int num_attention_heads, int ffn_embedding_dim)
+      : n_devices_(n_devices), queues(n_devices + 1) {
     for (int i = 0; i < n_devices; i++) {
-      int segment_size = n_layers / n_devices + (i < n_layers % n_devices);
+      int segment_size = n_layers / n_devices + int(i < n_layers % n_devices);
       auto gpt_segment = register_module("segment_" + std::to_string(i),
                                          std::make_shared<SingleDeviceGPT>(segment_size, embedding_dim,
                                                                            num_attention_heads, ffn_embedding_dim,
                                                                            torch::Device("cuda:" + std::to_string(i))));
       segments.push_back(std::move(gpt_segment));
+      workers[i] = std::thread(local_pipeline_stage, gpt_segment, std::ref(queues[i]), std::ref(queues[i + 1]),
+                               torch::Device("cuda:" + std::to_string(i)));
     }
   }
 
+  torch::Tensor forward(torch::Tensor x, int n_slices) {
+    queues[0].add(std::make_pair(0, torch::Tensor()));
+    int seq_len = x.size(0);
+    int pos = 0;
+    for (int i = 0; i < n_slices; i++) {
+      int subseq_len = seq_len / n_slices + int(i < seq_len % n_slices);
+      slice = x.index({torch::indexing::Slice(pos, pos + subseq_len)});
+      queues[0].add(std::make_pair(1, slice));
+    }
+    std::vector<torch::Tensor> results;
+    for (int i = 0; i < n_slices; i++) {
+      PacketType r;
+      queues.back().consume(r);
+      results.push_back(r.second);
+    }
+    return torch::cat(results, 0);
+  }
+
+  void stop() {
+    queues[0].add(std::make_pair(2, torch::Tensor()));
+    for (const auto& w: workers) {
+      w.join();
+    }
+  }
+
+  ~MultiDeviceGPT() {
+    stop();
+  }
+
+  int n_devices_;
   std::vector<std::shared_ptr<SingleDeviceGPT>> segments;
-  std::vector<ConsumerProducerQueue<std::pair<int, torch::Tensor>>> queues;
+  std::vector<PipelineDataQueue> queues;
+  std::vector<std::thread> workers;
 };
 
 int main() {
@@ -160,7 +228,7 @@ int main() {
 
   SingleDeviceGPT gpt_s(12, embedding_dim, 64, 3072 * 2, torch::Device("cuda:0"));
   auto r = gpt_s.forward(x, {});
-  auto fake_loss =  std::get<0>(r).mean();
+  auto fake_loss = std::get<0>(r).mean();
   std::cout << fake_loss << std::endl;
   fake_loss.backward();
   return 0;
