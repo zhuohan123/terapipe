96b45b8f5cec31e55d27d74b8d26bc08ae5a58eb
Zhuohan Li <zhuohan123@vip.qq.com>
2020-09-24 03:46:26 +0000

    update running script

--- run_ucx.sh
+++ run_ucx.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
-if [ "$#" -lt 2 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes required"; exit -1; fi
-if [ "$#" -gt 2 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
+if [ "$#" -lt 1 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes required"; exit -1; fi
+if [ "$#" -gt 1 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
 
 N_NODES=$1
 
 PYTHON_EXEC=/home/ubuntu/anaconda3/envs/ucx/bin/python
+PYTHON_SCRIPT=$(realpath -s test_transformer_ucx.py)
 ROOT_DIR=$(dirname $(realpath -s ${0}))
 source ${ROOT_DIR}/load_cluster_env.sh
 
 PREV_ADDR=""
 
-for i in $(seq ${N_NODES}); do
+echo ALL_IPADDR ${ALL_IPADDR[@]}
+
+for i in $(seq 0 $((N_NODES - 1))); do
   NODE_ADDR=${ALL_IPADDR[i]}
   echo ${i} "=>" ${NODE_ADDR}
   if [ ${i} == 0 ]; then
     ssh -o StrictHostKeyChecking=no ${NODE_ADDR} \
-       ${PYTHON_EXEC} test_transformer_ucx.py \
+       ${PYTHON_EXEC} ${PYTHON_SCRIPT} \
         --my-address ${NODE_ADDR} \
         --my-port 7777 \
     &
   elif [ ${i} == $((N_NODES - 1)) ]; then
     ssh -o StrictHostKeyChecking=no ${NODE_ADDR} \
-      ${PYTHON_EXEC} test_transformer_ucx.py \
+      ${PYTHON_EXEC} ${PYTHON_SCRIPT} \
         --prev-address ${PREV_ADDR} \
         --prev-port 7777 \
     &
   else
     ssh -o StrictHostKeyChecking=no ${NODE_ADDR} \
-      ${PYTHON_EXEC} test_transformer_ucx.py \
+      ${PYTHON_EXEC} ${PYTHON_SCRIPT} \
         --my-address ${NODE_ADDR} \
         --my-port 7777 \
         --prev-address ${PREV_ADDR} \
