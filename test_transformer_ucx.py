0957bb4044a884529830900f7b562257b64da80f
Zhuohan Li <zhuohan123@vip.qq.com>
2020-09-25 15:33:52 -0700

    move everything into a class

--- test_transformer_ucx.py
+++ test_transformer_ucx.py
@@ -13,19 +13,6 @@ from transformer_models import (
 )
 
 
-n_slices = 8
-q_in = queue.Queue()
-q_out = None
-loop = None
-config = TransformerConfig(
-    batch_size=1,
-    seq_len=1024,
-    n_layers=72,
-    embedding_dim=2048,
-    placement_orders=[0, 3, 2, 1, 5, 6, 7, 4],
-)
-
-
 def uniform_slice_x(x, n_slices):
     seq_len = x.size()[0]
     sliced_x = []
@@ -38,75 +25,97 @@ def uniform_slice_x(x, n_slices):
     return sliced_x
 
 
-async def put_stuff_to_q_out(x):
-    await q_out.put(x)
+class UCXTransformerRunner:
+    def __init__(self, config, n_slices, my_address, my_port, prev_address, prev_port):
+        self.config = config
+        self.n_slices = n_slices
+        self.my_address = my_address
+        self.my_port = my_port
+        self.prev_address = prev_address
+        self.prev_port = prev_port
+        self.comm = Communicator(self.ucx_main, my_address, my_port, prev_address, prev_port)
+        self.loop = self.comm.loop
+        self.q_in = queue.Queue()
+        self.q_out = asyncio.Queue(loop=self.loop)
+
+    async def ucx_main(self, prev_ep, next_ep):
+        await asyncio.wait([
+            self.recv_coroutine(prev_ep, next_ep),
+            self.send_coroutine(prev_ep, next_ep)
+        ])
+
+    async def recv_coroutine(self, prev_ep=None, next_ep=None):
+        x = self.config.create_inputs() if prev_ep is None else self.config.create_inputs_empty()
+        sliced_x = uniform_slice_x(x, self.n_slices)
+        for s in sliced_x:
+            if prev_ep is not None:
+                await prev_ep.recv(s)
+            self.q_in.put(s)
+
+        grad_x = self.config.create_inputs_empty()
+        sliced_grad_x = uniform_slice_x(grad_x, self.n_slices)
+        for s in reversed(sliced_grad_x):
+            if next_ep is not None:
+                await next_ep.recv(s)
+            self.q_in.put(s)
+
+    async def send_coroutine(self, prev_ep=None, next_ep=None):
+        for i in range(self.n_slices):
+            y = await self.q_out.get()
+            if next_ep is None:
+                print("forward i:", i, "y:", y, flush=True)
+            else:
+                await next_ep.send(y)
+
+        for i in reversed(range(self.n_slices)):
+            y = await self.q_out.get()
+            if prev_ep is not None:
+                await prev_ep.send(y)
+            else:
+                print("back i:", i, "y:", y, flush=True)
+
+    async def put_stuff_to_q_out(self, x):
+        await self.q_out.put(x)
+
+    def calc(self):
+        # forward
+        for i in range(self.n_slices):
+            x = self.q_in.get()
+            y = x + 1
+            asyncio.run_coroutine_threadsafe(self.put_stuff_to_q_out(y), loop=self.loop)
+
+        # backward
+        for i in reversed(range(self.n_slices)):
+            x = self.q_in.get()
+            y = x + 1
+            asyncio.run_coroutine_threadsafe(self.put_stuff_to_q_out(y), loop=self.loop)
+
+    def run(self):
+        t = threading.Thread(target=self.calc)
+        t.start()
+        self.comm.run()
+        t.join()
 
 
-def calc():
-    # forward
-    for i in range(n_slices):
-        x = q_in.get()
-        y = x + 1
-        asyncio.run_coroutine_threadsafe(put_stuff_to_q_out(y), loop=loop)
-
-    # backward
-    for i in reversed(range(n_slices)):
-        x = q_in.get()
-        y = x + 1
-        asyncio.run_coroutine_threadsafe(put_stuff_to_q_out(y), loop=loop)
-
-
-async def recv_coroutine(prev_ep=None, next_ep=None):
-    x = config.create_inputs() if prev_ep is None else config.create_inputs_empty()
-    sliced_x = uniform_slice_x(x, n_slices)
-    for s in sliced_x:
-        if prev_ep is not None:
-            await prev_ep.recv(s)
-        q_in.put(s)
-
-    grad_x = config.create_inputs_empty()
-    sliced_grad_x = uniform_slice_x(grad_x, n_slices)
-    for s in reversed(sliced_grad_x):
-        if next_ep is not None:
-            await next_ep.recv(s)
-        q_in.put(s)
-
-
-async def send_coroutine(prev_ep=None, next_ep=None):
-    for i in range(n_slices):
-        y = await q_out.get()
-        if next_ep is None:
-            print("forward i:", i, "y:", y, flush=True)
-        else:
-            await next_ep.send(y)
-
-    for i in reversed(range(n_slices)):
-        y = await q_out.get()
-        if prev_ep is not None:
-            await prev_ep.send(y)
-        else:
-            print("back i:", i, "y:", y, flush=True)
-
-
-async def ucx_main(prev_ep, next_ep):
-    await asyncio.wait([
-        recv_coroutine(prev_ep, next_ep),
-        send_coroutine(prev_ep, next_ep)
-    ])
-
-
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser(description='UCX based transformer')
     parser.add_argument('--my-address', metavar='IP', type=str, default=None)
     parser.add_argument('--my-port', metavar='PORT', type=int, default=None)
     parser.add_argument('--prev-address', metavar='IP', type=str, default=None)
     parser.add_argument('--prev-port', metavar='PORT', type=int, default=None)
     args = parser.parse_args()
-    comm = Communicator(ucx_main, args.my_address, args.my_port,
-                        args.prev_address, args.prev_port)
-    loop = comm.loop
-    q_out = asyncio.Queue(loop=loop)
-    t = threading.Thread(target=calc)
-    t.start()
-    comm.run()
-    t.join()
+
+    config = TransformerConfig(
+        batch_size=1,
+        seq_len=1024,
+        n_layers=72,
+        embedding_dim=2048,
+    )
+    n_slices = 8
+
+    runner = UCXTransformerRunner(config, n_slices, args.my_address, args.my_port, args.prev_address, args.prev_port)
+    runner.run()
+
+
+if __name__ == "__main__":
+    main()
