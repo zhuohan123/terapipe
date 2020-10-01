84b6f175bbcdda03ea0b32ccf2fc369cf4e91d64
Zhuohan Li <zhuohan123@vip.qq.com>
2020-10-01 03:50:11 +0000

    add pkill python

--- run_ucx.sh
+++ run_ucx.sh
@@ -10,7 +10,8 @@ PYTHON_SCRIPT=$(realpath -s test_transformer_ucx.py)
 ROOT_DIR=$(dirname $(realpath -s ${0}))
 source ${ROOT_DIR}/load_cluster_env.sh
 
-${ROOT_DIR}/fornode fuser -k 7777/tcp
+# ${ROOT_DIR}/fornode fuser -k 7777/tcp
+${ROOT_DIR}/fornode pkill python
 
 PREV_ADDR=""
 
