2237a1194ed1ca063185ff84e8ff015aed324813
Zhuohan Li <zhuohan123@vip.qq.com>
2020-10-14 22:57:59 -0700

    fix PYTHON_EXEC in load_cluster_env.sh

--- load_cluster_env.sh
+++ load_cluster_env.sh
@@ -1,10 +1,9 @@
 # This file should only be sourced.
 
-PYTHON_EXEC=python
 MY_IPADDR=$(hostname -i)
 # OTHERS_IPADDR=()
 # for s in $(ray get-worker-ips ~/ray_bootstrap_config.yaml); do
 #     OTHERS_IPADDR+=($(ssh -o StrictHostKeyChecking=no $s hostname -i));
 # done
-OTHERS_IPADDR=($($PYTHON_EXEC $(dirname $(realpath -s ${BASH_SOURCE[0]}))/get_worker_ips.py 2>/dev/null))
+OTHERS_IPADDR=($(python $(dirname $(realpath -s ${BASH_SOURCE[0]}))/get_worker_ips.py 2>/dev/null))
 ALL_IPADDR=($MY_IPADDR ${OTHERS_IPADDR[@]})
