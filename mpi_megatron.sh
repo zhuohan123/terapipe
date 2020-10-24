1bfe4942bdc3aaef7c2baf8e80c3c5b30a8da968
Zhuohan Li <zhuohan123@vip.qq.com>
2020-10-24 15:42:58 -0700

    Grid search runtime for different sequence length (#7)
    * distributed megatron
    
    * cluster settings
    
    * fix mpi
    
    * general interface for distributed megatron-lm
    
    * update config
    
    * fix arguments
    
    * fix bugs & add step time std
    
    * add grid search seq len
    
    * fix bug
    
    * fix args
    
    Co-authored-by: Siyuan <suquark@gmail.com>

---
+++ mpi_megatron.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$#" -lt 3 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes, model name, number of steps required"; exit -1; fi
+if [ "$#" -gt 3 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
+
+N_NODES=$1
+MODEL=$2
+N_STEPS=$3
+
+ROOT_DIR=$(dirname $(realpath -s ${0}))
+source ${ROOT_DIR}/load_cluster_env.sh
+all_hosts=$(echo ${ALL_IPADDR[@]:0:$N_NODES} | sed 's/ /,/g')
+PYTHON_EXEC=/home/ubuntu/anaconda3/envs/ucx/bin/python
+
+# ${ROOT_DIR}/fornode fuser -k 7777/tcp
+${ROOT_DIR}/fornode pkill python
+
+mpirun --mca btl_tcp_if_include ens3 --map-by ppr:1:node -H $all_hosts \
+  $PYTHON_EXEC test_megatron_distributed.py \
+    $MY_IPADDR \
+    -p 7777 \
+    --model $MODEL \
+    --n-steps $N_STEPS
+
--- test_transformer.py
+++ test_transformer.py
@@ -121,6 +121,43 @@ def grid_search_forward_time():
         print(n_layers, embedding_dim, seq_len, cache_times, this_step_time)
 
 
+def grid_search_seq_length_forward_time():
+    print("grid_search_seq_length_forward_time")
+    n_layers = 24
+    embedding_dim = 1024
+    seq_len_list = range(1, 1025)
+    ffn_embedding_dim = embedding_dim * 4
+    num_attention_heads = embedding_dim // 64
+    transformer_layers = [
+        TransformerLayer(embedding_dim, ffn_embedding_dim, num_attention_heads)
+        for _ in range(n_layers)
+    ]
+    batch_size = 1
+    assert embedding_dim % 64 == 0
+    single_device_transformer = SingleDeviceTransformer(transformer_layers).cuda(0)
+    time_testing_steps = 10
+    with torch.no_grad():
+        for seq_len in itertools.product(seq_len_list):
+            try:
+                x = torch.randn(seq_len, batch_size, embedding_dim).cuda(0)
+                # warm up
+                for t in range(2):
+                    single_device_transformer.zero_grad()
+                    y, _ = single_device_transformer(x)
+                torch.cuda.synchronize()
+                start = time.time()
+                for t in range(time_testing_steps):
+                    single_device_transformer.zero_grad()
+                    y, _ = single_device_transformer(x)
+                torch.cuda.synchronize()
+                duration = time.time() - start
+                this_step_time = duration / time_testing_steps
+            except:
+                this_step_time = None
+            print(seq_len, this_step_time, flush=True)
+
+
+
 def single_device_time(config: TransformerConfig, n_testing_steps=10):
     transformer_layers, x = config.create_layers_and_inputs()
     x = x.cuda(0)
@@ -276,21 +313,26 @@ def seqpipe_correctness(config: TransformerConfig, checkpoint_path, n_testing_st
     return duration / n_testing_steps
 
 
-def megatron_main(distributed_rank, distributed_init_method, distributed_world_size,
-                  config: TransformerConfig, n_testing_steps=10, profile=False):
+def megatron_main(local_rank, distributed_init_method, world_size,
+                  world_rank, config: TransformerConfig, n_testing_steps=10, profile=False):
+    local_size = torch.cuda.device_count()
+    distributed_world_size = world_size * local_size
+    distributed_world_rank = world_rank * local_size + local_rank
+    print("distributed_world_size", distributed_world_size, "distributed_world_rank", distributed_world_rank)
     dist.init_process_group(
         backend='nccl',
         init_method=distributed_init_method,
         world_size=distributed_world_size,
-        rank=distributed_rank,
+        rank=distributed_world_rank,
     )
-    torch.cuda.set_device(distributed_rank)
-    suppress_output(distributed_rank)
+    torch.cuda.set_device(local_rank)
+    suppress_output(distributed_world_rank)
     # perform a dummy all-reduce to initialize the NCCL communicator
     dist.all_reduce(torch.zeros(1).cuda())
     mpu.initialize_model_parallel(distributed_world_size)
     set_random_seed(0)
     mpu.model_parallel_cuda_manual_seed(0)
+    print("init_models")
     transformer_layers = [
         ModelParallelTransformerLayer(
             config.embedding_dim,
@@ -311,36 +353,42 @@ def megatron_main(distributed_rank, distributed_init_method, distributed_world_s
     torch.cuda.synchronize()
     print("start testing")
     start = time.time()
+
+    all_step_times = []
     with torch.autograd.profiler.profile(enabled=profile, use_cuda=True) as prof:
         for t in range(n_testing_steps):
+            start_time = time.time()
             transformer.zero_grad()
             y, _ = transformer(x)
             loss = torch.mean(y)
             loss.backward()
-        torch.cuda.synchronize()
+            torch.cuda.synchronize()
+            step_time = time.time() - start_time
+            all_step_times.append(step_time)
+            print("step_time:", step_time, flush=True)
     if profile:
         print("writing trace to disk")
         prof.export_chrome_trace("gpipe.gtrace")
         print(prof.key_averages().table())
     duration = time.time() - start
-    print("megatron (s/it):", duration / n_testing_steps)
+    print("megatron (s/it):", np.mean(all_step_times), np.std(all_step_times))
 
 
-def megatron_spawn_tasks(config, n_testing_steps=10):
-    port = random.randint(10000, 20000)
-    distributed_init_method = 'tcp://localhost:{port}'.format(port=port)
-    distributed_world_size = torch.cuda.device_count()
+def megatron_spawn_tasks(world_size, world_rank, ip_address, port, config, n_testing_steps=10):
+    distributed_init_method = f'tcp://{ip_address}:{port}'
+    local_size = torch.cuda.device_count()
     torch.multiprocessing.spawn(
         megatron_main,
-        args=(distributed_init_method, distributed_world_size, config, n_testing_steps),
-        nprocs=distributed_world_size
+        args=(distributed_init_method, world_size, world_rank, config, n_testing_steps),
+        nprocs=local_size,
     )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Different parallel methods for the Transformer')
     parser.add_argument('--type', metavar='NAME', type=str, default=None,
-                        choices=["gridsearch", "single", "correctness", "single_correctness", "gpipe", "seqpipe", "seqpipe_correctness", "megatron"])
+                        choices=["gridsearch", "gridseqlen", "single", "correctness", "single_correctness", "gpipe",
+                                 "seqpipe", "seqpipe_correctness", "megatron"])
     parser.add_argument('--model', metavar='NAME', type=str, default=None,
                         choices=list(MODEL_CONFIGS.keys()))
     parser.add_argument('--n-slices', metavar='N', type=int, default=8)
@@ -358,6 +406,8 @@ if __name__ == "__main__":
     )
     if args.type == "gridsearch":
         grid_search_forward_time()
+    elif args.type == "gridseqlen":
+        grid_search_seq_length_forward_time()
     elif args.type == "single":
         print("single_device (s/it):", single_device_time(config, n_testing_steps=args.n_steps))
     elif args.type == "correctness":
@@ -374,4 +424,4 @@ if __name__ == "__main__":
         assert args.checkpoint_path is not None
         seqpipe_correctness(config, args.checkpoint_path, n_testing_steps=args.n_steps, n_slices=args.n_slices)
     elif args.type == "megatron":
-        megatron_spawn_tasks(config, n_testing_steps=args.n_steps)
+        megatron_spawn_tasks(1, 0, 'localhost', random.randint(10000, 20000), config, n_testing_steps=args.n_steps)
