a083a44400093e45e9e1b1cf808d8df3b064bf51
Zhuohan Li <zhuohan123@vip.qq.com>
2020-11-02 14:30:12 -0800

    modify mpu to include pipeline info

--- mpu/__init__.py
+++ mpu/__init__.py
@@ -27,6 +27,7 @@ from .initialize import get_model_parallel_group
 from .initialize import get_model_parallel_rank
 from .initialize import get_model_parallel_src_rank
 from .initialize import get_model_parallel_world_size
+from .initialize import get_pipeline_parallel_group_rank
 from .initialize import initialize_model_parallel
 from .initialize import model_parallel_is_initialized
 
