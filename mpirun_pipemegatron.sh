5bb9735ccc0c8edabd4ff858d468c56d9d3d8403
Siyuan <suquark@gmail.com>
2020-11-12 17:31:38 -0800

    pipemegatron with MPI

---
+++ mpirun_pipemegatron.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ "$#" -lt 7 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) number of nodes, number of gpus per node, model parallel size, pipeline parallel size, model name, number of slices, number of steps, [extra args] required"; exit -1; fi
+if [ "$#" -gt 8 ]; then echo "$(tput setaf 1)[ERROR]$(tput sgr 0) too many arguments: $#"; exit -1; fi
+
+N_NODES=$1
+N_GPUS=$2 # per node
+MODEL_PARALLEL_SIZE=$3
+PIPELINE_PARALLEL_SIZE=$4
+MODEL=$5
+N_SLICES=$6
+N_STEPS=$7
+EXTRA_ARGS=$8
+
+PYTHON_EXEC=$(which python)
+PYTHON_SCRIPT=$(realpath -s test_transformer_pipemegatron.py)
+ROOT_DIR=$(dirname $(realpath -s ${0}))
+source ${ROOT_DIR}/scripts/load_cluster_env.sh
+
+# ${ROOT_DIR}/scripts/fornode fuser -k 7777/tcp
+${ROOT_DIR}/scripts/fornode pkill python
+
+echo ALL_IPADDR ${ALL_IPADDR[@]}
+all_hosts=$(echo ${ALL_IPADDR[@]:0:$N_NODES} | sed 's/ /,/g')
+
+# '--oversubscribe' enables MPI to run muliple processes per node.
+mpirun --oversubscribe --mca btl_tcp_if_include ens3 --map-by ppr:$N_GPUS:node -H $all_hosts \
+    ${PYTHON_EXEC} ${PYTHON_SCRIPT} \
+    $MY_IPADDR \
+    -p 7777 \
+    --model-parallel-size ${MODEL_PARALLEL_SIZE} \
+    --pipeline-parallel-size ${PIPELINE_PARALLEL_SIZE} \
+    --model ${MODEL} \
+    --n-slices ${N_SLICES} \
+    --n-steps ${N_STEPS} \
+    --use-mpi \
+    ${EXTRA_ARGS}
--- scripts/load_cluster_env.sh
+++ scripts/load_cluster_env.sh
@@ -1,9 +1,20 @@
 # This file should only be sourced.
 
 MY_IPADDR=$(hostname -i)
-# OTHERS_IPADDR=()
-# for s in $(ray get-worker-ips ~/ray_bootstrap_config.yaml); do
-#     OTHERS_IPADDR+=($(ssh -o StrictHostKeyChecking=no $s hostname -i));
-# done
-OTHERS_IPADDR=($(python $(dirname $(realpath -s ${BASH_SOURCE[0]}))/get_worker_ips.py 2>/dev/null))
-ALL_IPADDR=($MY_IPADDR ${OTHERS_IPADDR[@]})
+OTHERS_IPADDR=()
+
+# The part is does not depend on the Ray runtime.
+all_public_ips=$(ray get-worker-ips ~/ray_bootstrap_config.yaml)
+for s in $all_public_ips; do
+    # if [ ! -f /tmp/$s.ip ]; then
+    ssh -o StrictHostKeyChecking=no $s hostname -i > /tmp/$s.ip &
+    # fi
+done
+wait
+for s in $all_public_ips; do
+    OTHERS_IPADDR+=($(cat /tmp/$s.ip))
+done
+
+# # This part depends on Ray runtime. It could be slightly faster.
+# OTHERS_IPADDR=($(python $(dirname $(realpath -s ${BASH_SOURCE[0]}))/get_worker_ips.py 2>/dev/null))
+# ALL_IPADDR=($MY_IPADDR ${OTHERS_IPADDR[@]})
