59e4a9cb6af49b44a2637b5e3df5e4a195c08fa3
sguo35 <sguo35@gmail.com>
2020-11-23 07:41:39 +0000

    fix inplace transpose

--- test_transformer_pipemegatron.py
+++ test_transformer_pipemegatron.py
@@ -14,6 +14,7 @@ from transformer_models import (
     TransformerConfig, MODEL_CONFIGS, uniform_slice_x,
     ModelParallelTransformerLayer,
 )
+import gc
 
 WARM_UP_ROUNDS = 5
 LOSS_SCALE_FACTOR = 128.0
@@ -216,6 +217,8 @@ class NCCLTransformerRunner:
             start_time = time.time()
             self.step()
             step_time = time.time() - start_time
+            gc.collect()
+            torch.cuda.empty_cache()
             all_step_times.append(step_time)
             print("rank", self.rank, "step_time:", step_time, flush=True)
         if len(all_step_times) > WARM_UP_ROUNDS:
