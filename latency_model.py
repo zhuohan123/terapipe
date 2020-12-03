399eefc7877a4afe74a9f346b8b6039890894c40
Zhuohan Li <zhuohan123@vip.qq.com>
2020-12-03 14:04:26 -0800

    latency model (#33)

--- latency_model.py
+++ latency_model.py
@@ -1,4 +1,6 @@
 import json
+import time
+import numba
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -23,8 +25,17 @@ class SingleLayerLatency:
         attn_time = self.attn_cache_linear_model.predict([[seqlen, attn_cache_len, seqlen * attn_cache_len]])[0]
         return f_and_b_time + attn_time
 
-    def update_time():
+    def update_time(self):
         return self.update_time
+
+    def predict_latency_grid(self, total_length):
+        assert total_length % STEP_GAP == 0
+        n_seq_slices = total_length // STEP_GAP
+        grid = np.zeros((1 + n_seq_slices, 1 + n_seq_slices), dtype=np.float)
+        for seqlen in range(1, n_seq_slices + 1):
+            for cachelen in range(n_seq_slices + 1):
+                grid[seqlen, cachelen] = self.predict(seqlen * STEP_GAP, cachelen * STEP_GAP)
+        return grid
 
 
 def merge_dict(data):
@@ -77,7 +88,63 @@ def fit_single_layer_model(model_name):
     return SingleLayerLatency(f_plus_b, seqlen_latency['update_mean'], attention_cache_len_lrmodel)
 
 
+@numba.jit(nopython=True, parallel=True)
+def planning(latency_grid, seqlen, pipelinelen, max_latency):
+    assert seqlen % STEP_GAP == 0
+    n_seq_slices = seqlen // STEP_GAP
+    dp = np.zeros(n_seq_slices + 1, dtype=np.float64)
+    dp_step_len = np.zeros(n_seq_slices + 1, dtype=np.int64)
+    dp_actual_max_latency = np.zeros(n_seq_slices + 1, dtype=np.float64)
+    # DP[TOTAL_LENGTH]
+    for total_length in range(1, n_seq_slices + 1):
+        dp[total_length] = np.inf
+        for this_step_length in range(1, total_length + 1):
+            step_latency = latency_grid[this_step_length, total_length - this_step_length]
+            total_time = dp[total_length - this_step_length] + step_latency
+            if step_latency <= max_latency and total_time < dp[total_length]:
+                dp[total_length] = total_time
+                dp_step_len[total_length] = this_step_length
+                dp_actual_max_latency[total_length] = max(dp_actual_max_latency[total_length], step_latency)
+
+    final_time = (pipelinelen - 1) * dp_actual_max_latency[n_seq_slices] + dp[n_seq_slices]
+    if final_time == np.inf:
+        return final_time, None
+    slice_scheme = []
+    current_len = n_seq_slices
+    while current_len > 0:
+        this_step_len = dp_step_len[current_len]
+        slice_scheme.append(this_step_len * STEP_GAP)
+        current_len -= this_step_len
+    return final_time, slice_scheme
+
+
+def evaluate_split(latency_model, split_scheme, pipelinelen):
+    cache_len = 0
+    total_time = 0
+    largest_time = 0
+    for split in split_scheme:
+        split_time = latency_model.predict(split, cache_len)
+        total_time += split_time
+        cache_len += split
+        largest_time = max(largest_time, split_time)
+    return total_time + (pipelinelen - 1) * largest_time
+
 if __name__ == "__main__":
     # analysis_model('gpt3-175b')
     single_layer_model = fit_single_layer_model('gpt3-175b')
-    print(single_layer_model.predict(128, 32))
+    seqlen = 2048
+    pipelinelen = 48
+    time_grid = single_layer_model.predict_latency_grid(seqlen)
+    all_possible_latencies = np.sort(np.unique(time_grid))
+    best_latency = np.inf
+    best_scheme = None
+    for max_latency in all_possible_latencies:
+        if max_latency * pipelinelen > best_latency:
+            break
+        latency, split_scheme = planning(time_grid, seqlen, pipelinelen, max_latency)
+        if split_scheme is not None:
+            split_scheme = list(reversed(split_scheme))
+        if latency < best_latency:
+            best_latency = latency
+            best_scheme = split_scheme
+            print(best_latency, best_scheme, len(best_scheme))
