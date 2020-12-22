e9d53241bf22c20822ca3a7a88601f8b54c194b0
Shiyuan Guo <shiyuan.guo@berkeley.edu>
2020-12-21 20:54:10 -0800

    batch tester

---
+++ batch_test.py
@@ -0,0 +1,68 @@
+import os
+import subprocess
+import argparse
+
+from itertools import product
+
+from transformer_models import (
+    MODEL_CONFIGS
+)
+
+def parse_comma_delimited_arg(arg, cast_fn):
+    list_form = arg.split(',')
+    return list(map(cast_fn, list_form))
+
+def run_experiment(n_nodes, n_gpus_per_node, model_parallel_size, pipeline_parallel_size, 
+                model, batch_size, n_batch_slices, n_input_slices, n_steps, mixed_precision):
+    run_cmd = [
+        "bash",
+        "mpirun_pipemegatron.sh",
+        str(n_nodes),
+        str(n_gpus_per_node),
+        str(model_parallel_size),
+        str(pipeline_parallel_size),
+        str(model),
+        str(batch_size),
+        str(n_batch_slices),
+        str(n_input_slices),
+        str(n_steps),
+        (lambda x: "--mixed-precision" if x else '')(mixed_precision)
+    ]
+
+    p = subprocess.Popen(run_cmd, shell=True)
+    try:
+        # 2min timeout
+        p.wait(120)
+    except subprocess.TimeoutExpired as e:
+        os.system("./scripts/fornode pkill python")
+    except RuntimeError as e:
+        print(e)
+        os.system("./scripts/fornode pkill python")
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Pipeline + Megatron-LM runner')
+    parser.add_argument('--n-nodes', metavar='N', type=int, default=8)
+    parser.add_argument('--n-gpus-per-node', metavar='N', type=int, default=8)
+    parser.add_argument('--model-parallel-size', metavar='N', type=str, default='1')
+    parser.add_argument('--pipeline-parallel-size', metavar='N', type=str, default='1')
+    parser.add_argument('--model', metavar='NAME', type=str, default=None)
+    parser.add_argument('--batch-size', metavar='N', type=str, default='1')
+    parser.add_argument('--n-batch-slices', metavar='N', type=str, default='1')
+    parser.add_argument('--n-input-slices', metavar='N', type=str, default='1')
+    parser.add_argument('--n-steps', metavar='N', type=int, default=10)
+    parser.add_argument('--mixed-precision', action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    args.model_parallel_size = parse_comma_delimited_arg(args.model_parallel_size, lambda x: int(x))
+    args.pipeline_parallel_size = parse_comma_delimited_arg(args.pipeline_parallel_size, lambda x: int(x))
+    args.batch_size = parse_comma_delimited_arg(args.batch_size, lambda x: int(x))
+    args.n_batch_slices = parse_comma_delimited_arg(args.n_batch_slices, lambda x: int(x))
+    args.n_input_slices = parse_comma_delimited_arg(args.n_input_slices, lambda x: int(x))
+    args.model = parse_comma_delimited_arg(args.model, lambda x: str(x))
+
+    for experiment in product(args.model_parallel_size, args.pipeline_parallel_size, args.batch_size, args.n_batch_slices, args.n_input_slices, args.model):
+        model_parallel_size, pipeline_parallel_size, batch_size, n_batch_slices, n_input_slices, model = experiment
+        run_experiment(args.n_nodes, args.n_gpus_per_node, model_parallel_size, pipeline_parallel_size,
+            model, batch_size, n_batch_slices, n_input_slices, args.n_steps, args.mixed_precision)
