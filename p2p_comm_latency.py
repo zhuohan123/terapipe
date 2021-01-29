17f251f7740e2516b1c398d49dd7de4ddc0163d6
Siyuan <suquark@gmail.com>
2021-01-28 19:44:00 -0800

    show progress

--- p2p_comm_latency.py
+++ p2p_comm_latency.py
@@ -6,6 +6,7 @@ from mpi4py import MPI
 import numpy as np
 import torch
 import torch.distributed as dist
+import tqdm
 
 from transformer_models import MODEL_CONFIGS, BATCH_CONFIGS
 
@@ -32,6 +33,7 @@ def p2p_communication_latency(payload_size, group):
 
 
 def benchmark_p2p_communication(mpi_comm, rank, model_name, size_gap=8):
+    print(f"Measuring communication latency of {model_name}...", flush=True)
     _,  token_size, seqlen,  _ = MODEL_CONFIGS[model_name]
     max_tokens = seqlen * BATCH_CONFIGS[model_name]
     # we assume 2 8-GPU machines are used
@@ -40,15 +42,15 @@ def benchmark_p2p_communication(mpi_comm, rank, model_name, size_gap=8):
     durations_std = []
 
     tensor_sizes = list(range(size_gap, max_tokens + 1, size_gap))
-    for tokens in range(size_gap, max_tokens + 1, size_gap):
+    for tokens in tqdm.tqdm(tensor_sizes):
         payload_size = tokens * token_size
         mpi_comm.Barrier()
         if rank in (7, 8):
             mean, std = p2p_communication_latency(payload_size, send_recv_group)
             if rank == 7:
-                print(f"\npayload size = {payload_size}, " 
-                    f"communication finished in {mean*1000:.6f} ± {std*1000:.6f}ms",
-                    flush=True)
+                # print(f"\npayload size = {payload_size}, " 
+                #     f"communication finished in {mean*1000:.6f} ± {std*1000:.6f}ms",
+                #     flush=True)
                 durations_mean.append(mean)
                 durations_std.append(std)
 
