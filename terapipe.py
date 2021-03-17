0c9cc32602db9517cb80eae61cdae43ad3d3787f
Zhuohan Li <zhuohan123@vip.qq.com>
2021-03-17 16:30:02 -0700

    fix backward_hook with concat

--- terapipe.py
+++ terapipe.py
@@ -145,46 +145,9 @@ class PipelineRecvOperator(torch.autograd.Function):
         return None
 
 
-def terapipe_backward(outputs, grad_outputs, cache_inputs, cache_outputs):
-    n_batch_slices, n_input_slices = outputs.shape
-    for batch_id in reversed(range(n_batch_slices)):
-        da = []
-        for input_id in reversed(range(n_input_slices)):
-            y = outputs[batch_id, input_id]
-            dy = grad_outputs[batch_id, input_id]
-            if input_id < n_input_slices - 1:
-                a = cache_outputs[batch_id, input_id]
-            else:
-                a = []
-            torch.autograd.backward([y] + a, [dy] + da)
-            da = [t.grad for t in cache_inputs[batch_id, input_id]]
-
-
 class TeraPipeBackwardPassHook(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, backward_helper, outputs, cache_inputs, cache_outputs):
-        ctx.outputs = outputs
-        ctx.cache_inputs = cache_inputs
-        ctx.cache_outputs = cache_outputs
-        return backward_helper
-
-    @staticmethod
-    def backward(ctx, _):
-        n_batch_slices, n_input_slices = ctx.outputs.shape
-        grad_outputs = np.empty((n_batch_slices, n_input_slices), dtype='O')
-        for i in range(n_batch_slices):
-            for j in range(n_input_slices):
-                grad_outputs[i, j] = torch.empty_like(ctx.outputs[i, j])
-        terapipe_backward(ctx.outputs, grad_outputs, ctx.cache_inputs, ctx.cache_outputs)
-        del ctx.outputs
-        del ctx.cache_inputs
-        del ctx.cache_outputs
-        return None, None, None, None
-
-
-class ScatterWithTeraPipeBackwardPass(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, outputs, cache_inputs, cache_outputs, batch_slices, seq_slices, batch_dim, sequence_dim):
+    def forward(ctx, y, outputs, cache_inputs, cache_outputs, batch_slices, seq_slices, batch_dim, sequence_dim, cat_outputs):
         ctx.outputs = outputs
         ctx.cache_inputs = cache_inputs
         ctx.cache_outputs = cache_outputs
@@ -192,15 +155,35 @@ class ScatterWithTeraPipeBackwardPass(torch.autograd.Function):
         ctx.seq_slices = seq_slices
         ctx.batch_dim = batch_dim
         ctx.sequence_dim = sequence_dim
-        y = torch.cat([torch.cat(s.tolist(), dim=sequence_dim) for s in outputs], dim=batch_dim)
+        ctx.cat_outputs = cat_outputs
         return y
 
     @staticmethod
     def backward(ctx, grad_y):
-        grad_outputs = grid_slice_batch_and_sequence(
-            grad_y, batch_slices=ctx.batch_slices, seq_slices=ctx.seq_slices,
-            batch_dim=ctx.batch_dim, sequence_dim=ctx.sequence_dim, requires_grad=False)
-        terapipe_backward(ctx.outputs, grad_outputs, ctx.cache_inputs, ctx.cache_outputs)
+        n_batch_slices = len(ctx.batch_slices)
+        n_input_slices = len(ctx.seq_slices)
+        if ctx.cat_outputs:
+            grad_outputs = grid_slice_batch_and_sequence(
+                grad_y, batch_slices=ctx.batch_slices, seq_slices=ctx.seq_slices,
+                batch_dim=ctx.batch_dim, sequence_dim=ctx.sequence_dim, requires_grad=False)
+        else:
+            grad_outputs = np.empty((n_batch_slices, n_input_slices), dtype='O')
+            for i in range(n_batch_slices):
+                for j in range(n_input_slices):
+                    grad_outputs[i, j] = torch.empty_like(ctx.outputs[i, j])
+
+        for batch_id in reversed(range(n_batch_slices)):
+            da = []
+            for input_id in reversed(range(n_input_slices)):
+                y = ctx.outputs[batch_id, input_id]
+                dy = grad_outputs[batch_id, input_id]
+                if input_id < n_input_slices - 1:
+                    a = ctx.cache_outputs[batch_id, input_id]
+                else:
+                    a = []
+                torch.autograd.backward([y] + a, [dy] + da)
+                da = [t.grad for t in ctx.cache_inputs[batch_id, input_id]]
+
         del ctx.outputs
         del ctx.cache_inputs
         del ctx.cache_outputs
@@ -208,18 +191,21 @@ class ScatterWithTeraPipeBackwardPass(torch.autograd.Function):
         del ctx.seq_slices
         del ctx.batch_dim
         del ctx.sequence_dim
-        return None, None, None, None, None, None, None
+        del ctx.cat_outputs
+        return None, None, None, None, None, None, None, None, None
 
 
-def terapipe_backward_hook(outputs, cache_inputs, cache_outputs):
-    backward_helper = torch.tensor(0.0, requires_grad=True)
-    backward_helper = TeraPipeBackwardPassHook.apply(backward_helper, outputs, cache_inputs, cache_outputs)
-    return backward_helper
+def terapipe_backward_hook(outputs, cache_inputs, cache_outputs, batch_slices, seq_slices, batch_dim=1, sequence_dim=0, cat_outputs=False):
+    if cat_outputs:
+        y = torch.cat([torch.cat(s.tolist(), dim=sequence_dim) for s in outputs], dim=batch_dim)
+    else:
+        y = torch.tensor(0.0, requires_grad=True)
+    y = TeraPipeBackwardPassHook.apply(y, outputs, cache_inputs, cache_outputs, batch_slices, seq_slices, batch_dim, sequence_dim, cat_outputs)
+    return y
 
 
 pipeline_send = PipelineSendOperator.apply
 pipeline_recv = PipelineRecvOperator.apply
-scatter_with_terapipe_backward = ScatterWithTeraPipeBackwardPass.apply
 
 
 class TeraPipe(nn.Module):
@@ -269,12 +255,11 @@ class TeraPipe(nn.Module):
                 outputs[batch_id, input_id] = y
                 cache_len += slice_seq_len
 
-        if mpu.get_pipeline_parallel_group_rank() == mpu.get_pipeline_parallel_world_size() - 1:
-            y = scatter_with_terapipe_backward(
-                outputs, cache_inputs, cache_outputs, self.batch_slices,
-                self.seq_slices, self.batch_dim, self.sequence_dim)
-        else:
-            y = terapipe_backward_hook(outputs, cache_inputs, cache_outputs)
+        y = terapipe_backward_hook(
+            outputs, cache_inputs, cache_outputs, self.batch_slices,
+            self.seq_slices, self.batch_dim, self.sequence_dim,
+            cat_outputs=(mpu.get_pipeline_parallel_group_rank() == mpu.get_pipeline_parallel_world_size() - 1))
+
         return y
 
 
